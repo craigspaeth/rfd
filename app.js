17eb5a5291de83f4741ae0ca65c33586c0349e81
Craig Spaeth <craigspaeth@gmail.com>
2014-03-12 23:50:35 -0400

    fix

--- app.js
+++ app.js
@@ -6,16 +6,13 @@ var express = require('express'),
 app.set('views', __dirname + '/src');
 app.set('view engine', 'jade');
 app.get('/', function(req, res) {
-  switch (req.subdomains[0]) {
-    case 'video':
-      res.render('video');
-      break;
-    case 'design':
-      res.render('design');
-      break;
-    default:
-      res.render('index');
-  }
+  res.render('index');
+});
+app.get('/design', function(req, res) {
+  res.render('design');
+});
+app.get('/video', function(req, res) {
+  res.render('video');
 });
 app.use(express.static(__dirname + '/public'));
 app.use(express.favicon(__dirname + "/public/images/favicon.ico")); 
