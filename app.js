a6fc47f6423ce6b680ec2a67a3e86014c7e14110
Craig Spaeth <craigspaeth@gmail.com>
2014-03-15 11:43:26 -0400

    fixes

--- app.js
+++ app.js
@@ -3,6 +3,29 @@ var express = require('express'),
     port = process.env.PORT || 4000,
     app = express();
 
+app.locals.logoCaptions = [
+  "Logo design for business that provides serviced apartment accommodation to both corporate and leisure guests",
+  "Logo design for dairy company in Wisconsin",
+  "Logo design for children's ski company in Vail, Colorado",
+  "Logo design for skateboarding company in Salt Lake City, UT",
+  "Logo and skateboard design for skateboarding company in Salt Lake City, UT",
+  "Logo design for a new backyard beanbag-toss game company based in Saskatoon, Saskatchewan, Canada",
+  "Logo design for artisan jewelry design company",
+  "Logo design for a health coaching company",
+  "Logo design for naturopathic clinic specializing in digestive health",
+  "Logo design for makeup brand that fills scars and deep pores",
+  "Logo design for aftermarket auto parts dealer in Singapore",
+  "T-shirt design for startup company that sells guidance to other start-ups"
+];
+
+app.locals.filmCaptions = [
+  "HBO Documentary: Reagan, Assistant Online Editor, Film Credit",
+  "Alex Gibney Documentary: Magic Trip, Second Assistant Online Editor",
+  "HBO Television Show: TREME (Season Two), DI Conform- Main Titles",
+  "HBO Television Show: TREME (Season One), Dailies Assistant",
+  "Winner, Set to Screen Series Contest from Director Baz Luhrmann, Fox, & Apple, 2008"
+];
+
 app.set('views', __dirname + '/src');
 app.set('view engine', 'jade');
 app.get('/', function(req, res) {
